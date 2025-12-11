911bae7e8524aa21c0ab5e70fc203f1cd628ed4e
unknown <anildhami49@gmail.com>
2025-12-11 23:47:01 +0530

    Add web.config and fix app.py port for Azure

--- app.py
+++ app.py
@@ -244,4 +244,6 @@ def root():
     }), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # Get port from environment variable for Azure
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port, debug=False)
